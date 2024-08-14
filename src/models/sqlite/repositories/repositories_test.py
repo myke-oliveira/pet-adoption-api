58423d5ef56568d27d10911fbe7e507ebc508f95
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2024-08-14 21:53:18 +0000

    🚧 PROGRESS: pets_repository, delete method

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,10 +1,18 @@
+import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
 
 db_connection_handler.connect_to_db()
 
 
+@pytest.mark.skip(reason="Interation with database.")
 def test_list_pets():
     repo = PetsRepository(db_connection_handler)
     response = repo.list_pets()
     print(f"{response=}")
+
+
+def test_delete_pets():
+    name = "belinha"
+    repo = PetsRepository(db_connection_handler)
+    repo.delete_pets(name)
