3fdf56fdf053d23f94383040b5b2f75bab99cd5f
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2024-08-19 21:08:59 +0000

    🆕 NEW: people_repository

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,6 +1,7 @@
 import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
+from .people_repository import PeopleRepository
 
 db_connection_handler.connect_to_db()
 
@@ -17,3 +18,24 @@ def test_delete_pets():
     name = "belinha"
     repo = PetsRepository(db_connection_handler)
     repo.delete_pets(name)
+
+
+@pytest.mark.skip(reason="Interation with database.")
+def test_insert_people():
+    first_name = "test name"
+    last_name = "test last name"
+    age = 77
+    pet_id = 2
+
+    repo = PeopleRepository(db_connection_handler)
+    repo.insert_person(first_name, last_name, age, pet_id)
+
+
+@pytest.mark.skip(reason="Interation with database.")
+def test_get_person():
+    person_id = 1
+
+    repo = PeopleRepository(db_connection_handler)
+    response = repo.get_person(person_id)
+    print()
+    print(response)
